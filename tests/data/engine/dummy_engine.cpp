5a4ff2b18480ea7632d49f691874f6fac44c4026
disservin <45608332+Disservin@users.noreply.github.com>
2023-03-14 22:42:28 +0100

    Update dummy_engine.cpp

--- tests/data/engine/dummy_engine.cpp
+++ tests/data/engine/dummy_engine.cpp
@@ -16,10 +16,11 @@ int main()
     std::vector<std::string> moves = {"f2f3", "e7e5", "g2g4", "d8h4"};
     int moveIndex = 0;
 
+    string cmd;
     while (true)
     {
-        string cmd;
-        getline(cin, cmd);
+        if (!getline(cin, cmd) && cin.eof())
+            cmd = "quit";
 
         if (cmd == "quit")
         {
